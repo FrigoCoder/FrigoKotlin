bb9d607549bd7f2d951afbb2ff1e24d9297a061f
Frigo <frigocoder@gmail.com>
2019-01-08 20:45:40 +0100

    I am trying to get camera working.

--- src/main/kotlin/trace/Main.kt
+++ src/main/kotlin/trace/Main.kt
@@ -4,15 +4,15 @@ import kotlin.random.Random
 
 fun main(args: Array<String>) {
     val scene = Scene(listOf(
-        Sphere(1e5, Vector(1e5 + 1, 40.8, 81.6), Color(0.0, 0.0, 0.0), Color(.75, .25, .25), Reflection.Diffuse),
-        Sphere(1e5, Vector(-1e5 + 99, 40.8, 81.6), Color(0.0, 0.0, 0.0), Color(.25, .25, .75), Reflection.Diffuse),
-        Sphere(1e5, Vector(50.0, 40.8, 1e5), Color(0.0, 0.0, 0.0), Color(.75, .75, .75), Reflection.Diffuse),
-        Sphere(1e5, Vector(50.0, 40.8, -1e5 + 170), Color(0.0, 0.0, 0.0), Color(0.0, 0.0, 0.0), Reflection.Diffuse),
-        Sphere(1e5, Vector(50.0, 1e5, 81.6), Color(0.0, 0.0, 0.0), Color(.75, .75, .75), Reflection.Diffuse),
-        Sphere(1e5, Vector(50.0, -1e5 + 81.6, 81.6), Color(0.0, 0.0, 0.0), Color(.75, .75, .75), Reflection.Diffuse),
-        Sphere(16.5, Vector(27.0, 16.5, 47.0), Color(0.0, 0.0, 0.0), Color(1.0, 1.0, 1.0) * .999, Reflection.Specular),
-        Sphere(16.5, Vector(73.0, 16.5, 78.0), Color(0.0, 0.0, 0.0), Color(1.0, 1.0, 1.0) * .999, Reflection.Refractive),
-        Sphere(600.0, Vector(50.0, 681.6 - .27, 81.6), Color(12.0, 12.0, 12.0), Color(0.0, 0.0, 0.0), Reflection.Diffuse)
+        Sphere(1e5, Vector(1e5 + 1, 40.8, 81.6), Color.Black, Color(.75, .25, .25), Reflection.Diffuse),
+        Sphere(1e5, Vector(-1e5 + 99, 40.8, 81.6), Color.Black, Color(.25, .25, .75), Reflection.Diffuse),
+        Sphere(1e5, Vector(50.0, 40.8, 1e5), Color.Black, Color(.75, .75, .75), Reflection.Diffuse),
+        Sphere(1e5, Vector(50.0, 40.8, -1e5 + 170), Color.Black, Color.Black, Reflection.Diffuse),
+        Sphere(1e5, Vector(50.0, 1e5, 81.6), Color.Black, Color(.75, .75, .75), Reflection.Diffuse),
+        Sphere(1e5, Vector(50.0, -1e5 + 81.6, 81.6), Color.Black, Color(.75, .75, .75), Reflection.Diffuse),
+        Sphere(16.5, Vector(27.0, 16.5, 47.0), Color.Black, Color.White * .999, Reflection.Specular),
+        Sphere(16.5, Vector(73.0, 16.5, 78.0), Color.Black, Color.White * .999, Reflection.Refractive),
+        Sphere(600.0, Vector(50.0, 681.6 - .27, 81.6), Color(12.0, 12.0, 12.0), Color.Black, Reflection.Diffuse)
     ))
 
     val image = Image(1024, 768)
