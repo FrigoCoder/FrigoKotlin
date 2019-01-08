d60231348121a1ab42bb4903daedec7d68e6fc82
Frigo <frigocoder@gmail.com>
2019-01-08 17:40:26 +0100

    Ray: Changed Ray from a data class to a class that automatically normalizes direction vector.

--- src/main/kotlin/trace/Ray.kt
+++ src/main/kotlin/trace/Ray.kt
@@ -1,3 +1,11 @@
 package trace
 
-data class Ray(val origin: Vector, val direction: Vector)
+class Ray(val origin: Vector, direction: Vector) {
+
+    val direction: Vector = direction.normalize()
+
+    override fun equals(other: Any?) = other is Ray && origin == other.origin && direction == other.direction
+    override fun hashCode() = 31 * origin.hashCode() + direction.hashCode()
+    override fun toString() = "Ray(origin=$origin, direction=$direction)"
+
+}
